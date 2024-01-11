1fa82e677d7c2eb278bff797bf681d8dd944d71d
Aptura <aptura@users.noreply.github.com>
2024-01-11 16:04:40 +0100

    reactivating the bot

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const path = require("node:path");
 const { Client, Collection, Events, GatewayIntentBits } = require("discord.js");
 const { token } = require("./config.json");
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({ intents: 3276799 });
 
 client.once(Events.ClientReady, (c) => {
   console.log(`Ready! Logged in as ${c.user.tag}`);
